fbfa74d7500f3bd8599b0413b5d356a8aaa7636f
Michel Brassard <michelbrassard47@gmail.com>
2025-12-06 13:49:28 +0100

    read one number from terminal

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -28,36 +28,27 @@ void setup() {
   servo4.write(0);
   servo5.write(0);
 
-  delay(3000);
+  delay(2000);
 
   Serial.println("Started");
 }
 
 void loop() {
   while (Serial.available() > 0) {
-    int input_char = Serial.read();
+    int number = Serial.parseInt();
 
-    if (isDigit(input_char)) {
-      
-      int degrees = (input_char - 48) * 10;
-      Serial.println("Rotate for 3 seconds...");
-      servo1.write(degrees);
-      servo2.write(degrees);
-      servo3.write(degrees);
-      servo4.write(degrees);
-      servo5.write(degrees);
-      delay(3000);
-      Serial.println("Back to zero");
-      servo1.write(0);
-      servo2.write(0);
-      servo3.write(0);
-      servo4.write(0);
-      servo5.write(0);
-      delay(3000);
+    Serial.print("Entered: ");
+    Serial.println(number);
+
+    if (number == 0 || number > 180) {
+      return;
     }
-    
 
-    
+    servo1.write(number);
+    servo2.write(number);
+    servo3.write(number);
+    servo4.write(number);
+    servo5.write(number);
   }
 }
 
